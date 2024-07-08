146695a5e9bc32bdb4f6bdb1380726123a9460d4
munseongyun <borygashill608@gmail.com>
2024-07-09 00:02:02 +0900

    ✅ Test : 이미지 테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [info,setInfo] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:3001/information",{
+      method:"GET"
+    }).then(res=>res.json()).then((res)=>setInfo(res))
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pup Patrol</h1>
+        {info.map((item)=>{
+          return(
+            <img src={item.information_image_url} width={"500px"}></img>
+          )
+        })}
       </header>
     </div>
   );
