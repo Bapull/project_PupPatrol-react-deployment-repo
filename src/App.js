3d6a46e2f337c1e548d8f914a63d38dc0c3ec935
mCaHtA <cjhms002@naver.com>
2024-07-10 21:06:06 +0900

    💫Edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { MobileView } from 'react-device-detect';
-import './App.css';
+import { BrowserView, MobileView } from "react-device-detect";
+import React from "react";
+import "./App.css";
+import CategorySelect from "./pages/CategorySelect.jsx";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MobileView>모바일 브라우져!</MobileView>
+      <BrowserView>
+        <Link to="/categorySelect">카테고리</Link>
+      </BrowserView>
+      <MobileView>
+        <Link to="/categorySelect">카테고리</Link>
+      </MobileView>
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/categorySelect"
+            element={<CategorySelect />}
+            exact={true}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/CategorySelect.jsx
+++ src/pages/CategorySelect.jsx
@@ -1,1 +1,12 @@
 // '키우고싶어요' '키우고있어요' 선택 페이지
+import React from 'react';
+
+function CategorySelect() {
+  return (
+    <div className="CategorySelect">
+      안녕!
+    </div>
+  );
+}
+
+export default CategorySelect;
