a460ad6ff262c7871d99a4c456e6110615e9ffa2
dlat.th <dlat.th45@gmail.com>
2024-07-11 21:07:57 +0900

    💡Feat : Add initial layout and styles

--- src/pages/WantSelect.jsx
+++ src/pages/WantSelect.jsx
@@ -1,11 +1,21 @@
 // 멍비티아이 진입, 강아지 종 검색 페이지
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/WantSelect.css';
 
 function WantSelect() {
   return (
-    <div className="WantSelect">
-      <h1>강아지 종 검색</h1>
+    <div className="wantSelect">
+      <Link to="/wantMBTI">
+        <div className="mbti-box">
+          <h1>DBTI</h1>
+        </div>
+      </Link>
+      <Link to="/dogSearch">
+        <div className="search-box">
+          <input type="text" placeholder="Search" />
+        </div>
+      </Link>
     </div>
   );
 }
