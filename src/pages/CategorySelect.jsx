fc89cf41beb0ebb08d5fb7f62095b4bc53ae14e9
mCaHtA <cjhms002@naver.com>
2024-07-12 01:54:56 +0900

    🎨Style : 코드 스타일 수정

--- src/pages/CategorySelect.jsx
+++ src/pages/CategorySelect.jsx
@@ -1,1 +1,27 @@
 // '키우고싶어요' '키우고있어요' 선택 페이지
+import React, { useState } from 'react';
+import style from '../styles/Category_Select.css';
+
+function CategorySelect() {
+  return (
+    <div className="CategorySelect">
+      <div class="wrapper">
+        <div class="content">
+          <h5>
+            당신은
+            <br />
+            강아지와 함께 살고 계십니까?
+            <div className="horizontal" /> {/* 밑줄 부분 (텍스트 크기 따라감! 굿!) */}
+          </h5>
+
+          <div className="buttonBox">
+            <button className="button1">YES</button>
+          </div>
+          <p>아니라면 오른쪽으로 슬라이드</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CategorySelect;
