4af00d60e29c5e585498b686f2db304532910f5a
dlat.th <dlat.th45@gmail.com>
2024-07-12 15:54:51 +0900

    💡Create : WantDogDescription.jsx, .css

---
+++ src/pages/WantDogDescription.jsx
@@ -0,0 +1,9 @@
+// 강아지 상세 설명 페이지
+import React from 'react';
+import '../styles/WantDogDescription.css';
+
+function WantDogDescription() {
+  return <h1>강아지 상세 설명 페이지</h1>;
+}
+
+export default WantDogDescription;
