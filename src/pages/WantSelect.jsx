22ad11247a358a7ddd34d49f598fa1b4b5a19dae
dlat.th <dlat.th45@gmail.com>
2024-07-15 14:43:27 +0900

    🎨Style : Navigate method & Add image

--- src/pages/WantSelect.jsx
+++ src/pages/WantSelect.jsx
@@ -1,23 +1,18 @@
 // 멍비티아이 진입, 강아지 종 검색 페이지
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/WantSelect.css';
 
 function WantSelect() {
-  const [expand, setExpand] = useState(false);
   const navigate = useNavigate();
-  const inputRef = useRef(null);
 
   const handleClickMBTI = () => {
-    setExpand(true);
     setTimeout(() => {
       navigate('/wantMBTI');
     }, 300);
   };
 
   const handleClickSearch = () => {
-    setExpand(true);
-    inputRef.current.focus();
     setTimeout(() => {
       navigate('/dogSearch');
     }, 300);
@@ -26,10 +21,10 @@ function WantSelect() {
   return (
     <div className="wantSelect">
       <div className="wantSelect_mbti-box" onClick={handleClickMBTI}>
-        <h1>DBTI</h1>
+        <h1 className="mbti-title">DBTI</h1>
       </div>
-      <div className={`wantSelect_search-box ${expand ? 'expand' : ''}`} onClick={handleClickSearch}>
-        <input ref={inputRef} type="text" placeholder="Search" />
+      <div className="wantSelect_search-box" onClick={handleClickSearch}>
+        <h1>Search Dogs</h1>
       </div>
     </div>
   );
