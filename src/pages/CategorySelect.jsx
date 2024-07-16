9388f11036aece0b92da308deab0a216885543f1
mCaHtA <cjhms002@naver.com>
2024-07-16 14:03:15 +0900

    💫Edit : button style, br delete, yes button position change

--- src/pages/CategorySelect.jsx
+++ src/pages/CategorySelect.jsx
@@ -1,24 +1,65 @@
 // '키우고싶어요' '키우고있어요' 선택 페이지
-import React, { useState } from 'react';
+import { useState, useEffect } from "react";
 import style from '../styles/Category_Select.css';
 
 function CategorySelect() {
+  const [position, setPosition] = useState(0);
+  const [start, setStart] = useState(0);
+
+  const oneTouch = () => {
+    document.querySelector('.content').style.transform = 'translate(0vw)';
+    document.querySelector('.button1').style.transform = 'scale(1.5)';
+    document.querySelector('.button2').style.transform = 'scale(1)';
+    document.querySelector('.button1').style.backgroundColor = 'white';
+    document.querySelector('.button2').style.backgroundColor = 'rgb(50, 50, 50)';
+  }
+  const twoTouch = () => {
+    document.querySelector('.content').style.transform = 'translate(-100vw)';
+    document.querySelector('.button2').style.transform = 'scale(1.5)';
+    document.querySelector('.button1').style.transform = 'scale(1)';
+    document.querySelector('.button1').style.backgroundColor = 'rgb(50, 50, 50)';
+    document.querySelector('.button2').style.backgroundColor = 'white';
+    
+  }
+
+  const onTouchStart = (e) => {
+    setStart(e.touches[0].clientX)
+  }
+  const onTouchMove = (e) => {
+    setPosition(e.touches[0].clientX)
+    if ( start - position > 10) {
+      twoTouch()
+    } else if ( start - position < -10) {
+      oneTouch()
+    }
+  }
+
   return (
     <div className="CategorySelect">
-      <div class="wrapper">
+      <div class="buttonGroup">
+        <button class="button1" onTouchEnd={oneTouch} onClick={oneTouch}></button>
+        <button class="button2" onTouchEnd={twoTouch} onClick={twoTouch}></button>
+      </div>
+      <div class="wrapper" onTouchStart={onTouchStart} onTouchMove={onTouchMove}>
         <div class="content">
-          <h5>
-            당신은
-            <br />
-            강아지와 함께 살고 계십니까?
-            <div className="horizontal" /> {/* 밑줄 부분 (텍스트 크기 따라감! 굿!) */}
-          </h5>
-
-          <div className="buttonBox">
-            <button className="button1">YES</button>
+          <div class="choice">
+            <div className="textBox">
+              <h5>
+                당신은 현재 강아지를 
+                <span class="choiceHave">당신은 현재 강이지를</span>
+              </h5>
+            </div>
+              <div>
+                <h5>
+                  키우고 있습니까?
+                  <span className="choiceHave">키우고 있지 않습니까?</span>
+                </h5>
+              </div>
+            </div>
           </div>
-          <p>아니라면 오른쪽으로 슬라이드</p>
-        </div>
+      </div>
+      <div className="buttonBox">
+        <button className="button3">YES</button>
       </div>
     </div>
   );
