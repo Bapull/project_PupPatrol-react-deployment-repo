0a286996583c4721cf9e7357ed7079bf456aea40
mCaHtA <cjhms002@naver.com>
2024-07-29 21:41:40 +0900

    💫Edit : useContext Changes by application

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import WantSelect from "./pages/WantSelect";
-import WantMBTI from "./pages/WantMBTI";
-import DogSearch from "./pages/DogSearch";
 import CategorySelect from "./pages/CategorySelect";
-import WantDogDescription from "./pages/WantDogDescription";
-import WantMBTIResult from "./pages/WantMBTIResult";
-import "./App.css";
+import DogSearch from "./pages/DogSearch";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Personal from "./pages/Personal";
+import Register from "./pages/Register";
+import WantDogDescription from "./pages/WantDogDescription";
+import WantMBTI from "./pages/WantMBTI";
+import WantMBTIResult from "./pages/WantMBTIResult";
+import WantSelect from "./pages/WantSelect";
+import { ApiProvider } from "./contexts/ApiContext";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <BrowserView></BrowserView>
-      <MobileView></MobileView>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/Personal" element={<Personal />}/>
-        <Route path="/categorySelect/*" element={<CategorySelect />} />
-        <Route path="/wantSelect" element={<WantSelect />} />
-        <Route path="/wantMBTI" element={<WantMBTI />} />
-        <Route path="/dogSearch" element={<DogSearch />} />
-        <Route path="/wantDogDescription" element={<WantDogDescription />} />
-        <Route path="/wantMBTIResult" element={<WantMBTIResult />} />
-      </Routes>
-    </Router>
+    <ApiProvider>
+      <Router>
+        <BrowserView></BrowserView>
+        <MobileView></MobileView>
+        <Routes>
+          <Route path="/" element={<Navigate to="/categorySelect" />} />
+          <Route path="/categorySelect/*" element={<CategorySelect />} />
+          <Route path="/dogSearch" element={<DogSearch />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/wantDogDescription" element={<WantDogDescription />} />
+          <Route path="/wantMBTI" element={<WantMBTI />} />
+          <Route path="/wantMBTIResult" element={<WantMBTIResult />} />
+          <Route path="/wantSelect" element={<WantSelect />} />
+        </Routes>
+      </Router>
+    </ApiProvider>
   );
 }
 
--- src/pages/WantDogDescription.jsx
+++ src/pages/WantDogDescription.jsx
@@ -1,19 +1,33 @@
 // 강아지 상세 설명 페이지
-import React from 'react';
-import '../styles/WantDogDescription.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import BackButton from '../components/backButton';
+import React from "react";
+import "../styles/WantDogDescription.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function WantDogDescription() {
   const location = useLocation();
   const { dog } = location.state;
   const navigate = useNavigate();
 
+  // 뒤로가기 버튼 클릭 시, 뒤로 이동
+  const handleClickBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="wantDogDescription">
       <div className="container">
-        <BackButton />
-        <img src={dog.informationImageUrl} alt="dogImage" className="background-image" />
+        <div className="backButtonBox" onClick={handleClickBack}>
+          <img
+            src="/images/Arrow.png"
+            alt="backButton"
+            className="backButton"
+          />
+        </div>
+        <img
+          src={dog.informationImageUrl}
+          alt="Dog_Image"
+          className="background-image"
+        />
         <div className="text-overlay">
           <h1>{dog.informationDogName}</h1>
         </div>
@@ -26,7 +40,7 @@ function WantDogDescription() {
         <h4>이 병에 대해선 조심해야해요!</h4> <br />
         <h5>{dog.informationDogGeneticillness}</h5>
         <h4>주의점!</h4> <br />
-        <h5>{dog.informationCaution}</h5>
+        <h5>{dog.information_caution}</h5>
       </div>
     </div>
   );
