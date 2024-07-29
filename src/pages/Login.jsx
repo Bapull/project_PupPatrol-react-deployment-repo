a3386975c8173ea8079bf77ca66c2cba4ce1fdbe
mCaHtA <cjhms002@naver.com>
2024-07-29 23:19:19 +0900

    🐛Fix : #26 add Login & SignIn by BackButton and edit BackButton css position

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,60 @@
-import React,{useState} from 'react'
-import '../styles/Login.css'
-import { useNavigate } from 'react-router-dom'
-import {loginApi} from '../utils/fetchAPI'
+import React, { useState } from "react";
+import "../styles/Login.css";
+import { useNavigate } from "react-router-dom";
+import { loginApi } from "../utils/fetchAPI";
+import BackButton from "../components/backButton";
 const Login = () => {
   const navigate = useNavigate();
   const [inputs, setInputs] = useState({
-    email:"",
-    password:""
-  })
-  const [error, setError] = useState("")
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
   const onChange = (e) => {
-    let {name, value} = e.target
-    setInputs((prev)=>{
+    let { name, value } = e.target;
+    setInputs((prev) => {
       return {
         ...prev,
-        [name]:value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
   const onSubmit = (e) => {
     e.preventDefault();
     loginApi("http://localhost:8000/api/login", inputs)
-    .then(()=>{
-      navigate("/personal")
-    }).catch(
-      (e)=>setError("아이디나 비밀번호가 잘못되었습니다.")
-    )
-  }
+      .then(() => {
+        navigate("/personal");
+      })
+      .catch((e) => setError("아이디나 비밀번호가 잘못되었습니다."));
+  };
   return (
-    <form onSubmit={onSubmit} className='container'>
-      <input type="email" name='email' value={inputs.email} onChange={onChange} placeholder=" Email" className='loginInput'/>
-      <input type="password" name='password' value={inputs.password} onChange={onChange} placeholder=" Password" className='loginInput' minLength={8}/>
-      <button type='submit' className='loginButton'>Login</button>
+    <form onSubmit={onSubmit} className="container">
+      <BackButton />
+      <input
+        type="email"
+        name="email"
+        value={inputs.email}
+        onChange={onChange}
+        placeholder=" Email"
+        className="loginInput"
+      />
+      <input
+        type="password"
+        name="password"
+        value={inputs.password}
+        onChange={onChange}
+        placeholder=" Password"
+        className="loginInput"
+        minLength={8}
+      />
+      <button type="submit" className="loginButton">
+        Login
+      </button>
       <p>{error}</p>
       <p>계정이 없으신가요?</p>
       <a href="/register">Sign in</a>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
