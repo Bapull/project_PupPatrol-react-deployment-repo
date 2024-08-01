aeec4c1581278384d74fff8d710ca02575fbc9c7
munseongyun <borygashill608@gmail.com>
2024-08-01 20:14:45 +0900

    💡Feat : fetchAPI image update

--- src/pages/ImageTest.jsx
+++ src/pages/ImageTest.jsx
@@ -1,55 +1,36 @@
 import React, { useState } from 'react'
-
+import {imageUploadApi, imageDownloadApi, imageDeleteApi} from '../utils/fetchAPI'
 const ImageTest = () => {
   const [image,setImage] = useState(null);
+  const [uploadedImageName, setUploadedImageName] = useState('')
   const [imageName, setImageName] = useState('')
   const [deleteImageName, setDeleteImageName] = useState('')  
   const [downloadedImage, setDownloadedImage] = useState('')
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('image',image)
-
-    fetch("http://localhost:8000/api/imageUpload",{
-      method:"POST",
-      headers:{
-        'folder':'pup-patrol-board-image'
-        // 'folder':'pup-patrol-information-image'
-      },
-      body:formData
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('error',error))
+    // 이미지 요청 보내는 법입니다.
+    // imageUploadApi는 url, 폴더 이름, 이미지 파일을 파라미터로 받습니다.
+    // 폴더이름은 board, information 2개가 있습니다.
+    const response = await imageUploadApi("http://localhost:8000/api/imageUpload",'board',image)
+    setUploadedImageName(await response.data)
   }
   
-  const onDownload = (e) =>{
+  const onDownload = async (e) =>{
     e.preventDefault();
-    fetch(`http://localhost:8000/api/imageDownload?fileName=${imageName}`,{
-      method:"GET",
-      headers:{
-        'Content-Type': 'application/json',
-        'folder':'pup-patrol-board-image'
-        // 'folder':'pup-patrol-information-image'
-      }
-    })
-    .then(response => response.json())
-    .then(data => setDownloadedImage(data.data))
-    .catch(error => console.error('Error:', error));
+    // 올린 이미지를 다운 받는 법입니다.
+    // imageDownloadApi는 url, 폴더 이름, 파일이름을 파라미터로 받습니다.
+    // 파일이름은 imageUploadApi의 리턴값을 사용해야합니다.
+    const response = await imageDownloadApi("http://localhost:8000/api/imageDownload","board",imageName)
+    console.log(response);
+    setDownloadedImage(response)
   }
-  const onDelete = (e) => {
+  const onDelete = async (e) => {
     e.preventDefault();
-    fetch(`http://localhost:8000/api/imageDelete?fileName=${deleteImageName}`, {
-      method: 'DELETE',
-      headers: {
-          'Content-Type': 'application/json',
-          'folder':'pup-patrol-board-image'
-        // 'folder':'pup-patrol-information-image'
-      },
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    // 올린 이미지를 삭제하는 법입니다.
+    // imageDeleteApi는 url, 폴더 이름, 파일이름을 파라미터로 받습니다.
+    // 파일이름은 imageUploadApi의 리턴값을 사용해야합니다.
+    const response = await imageDeleteApi(`http://localhost:8000/api/imageDelete`,"board",deleteImageName)
+    console.log(response.data);
   };
   return (
     <div>
@@ -57,6 +38,7 @@ const ImageTest = () => {
         <input type="file" onChange={(e)=>setImage(e.target.files[0])}/>
         <button type='submit'>업로드</button>
       </form>
+      <h1>업로드된 파일 명: {uploadedImageName}</h1>
       <form action="submit" onSubmit={onDownload}>
         <input type="text" onChange={(e)=>setImageName(e.target.value)}/>
         <img src={downloadedImage}  alt="downloaded" />
--- src/utils/fetchAPI.js
+++ src/utils/fetchAPI.js
@@ -200,3 +200,67 @@ export const deleteApi = async (url) => {
   const data = await response.json();
   return data;
 }
+
+export const imageUploadApi = async (url,folder, image) => {
+  const formData = new FormData();
+  formData.append('image',image)
+  let folderName = ''
+  if(folder === 'board'){
+    folderName = 'pup-patrol-board-image'
+  }else{
+    folderName = 'pup-patrol-information-image'
+  }
+  const response = await fetch(url, {
+    method:"POST",
+    headers: {
+      'folder': folderName,
+    },
+    body:formData
+  });
+  if(!response.ok){
+    throw new Error('Failed to fetch')
+  }
+  const data = await response.json();
+  return data;
+}
+
+export const imageDownloadApi = async ( url, folder, fileName) => {
+  let folderName = ''
+  if(folder === 'board'){
+    folderName = 'pup-patrol-board-image'
+  }else{
+    folderName = 'pup-patrol-information-image'
+  }
+  const response = await fetch(`${url}?fileName=${fileName}`, {
+    method:"GET",
+    headers: {
+      "Accept":"application/json",
+      'folder': folderName,
+    },
+  });
+  if(!response.ok){
+    throw new Error('Failed to fetch')
+  }
+  const data = await response.json();
+  return data.data;
+} 
+export const imageDeleteApi = async ( url, folder, fileName )=>{
+  let folderName = ''
+  if(folder === 'board'){
+    folderName = 'pup-patrol-board-image'
+  }else{
+    folderName = 'pup-patrol-information-image'
+  }
+  const response = await fetch(`${url}?fileName=${fileName}`, {
+    method:"DELETE",
+    headers: {
+      "Accept":"application/json",
+      'folder': folderName,
+    },
+  });
+  if(!response.ok){
+    throw new Error('Failed to fetch')
+  }
+  const data = await response.json();
+  return data;
+}
