39070e7fd080284d4230058c0e01b9a8417905ab
mCaHtA <cjhms002@naver.com>
2024-09-08 20:49:52 +0900

    💫Edit : style Diary

--- src/pages/board/BoardDescription.jsx
+++ src/pages/board/BoardDescription.jsx
@@ -21,20 +21,31 @@ const PrintTest = () => {
     <div className="boardDescription">
       <BackButton />
       <Profile />
-      <h1>{state.postTitle}</h1>
-      {post.map((item) => {
-        if (item.substr(0, 7) === "(IMAGE)") {
-          return (
-            <Image
-              folder={"board"}
-              fileName={item.substr(7)}
-              style={{ width: "200px" }}
-            />
-          );
-        } else {
-          return <p>{item}</p>;
-        }
-      })}
+      <h1 className="title">{state.postTitle}</h1>
+      <p className="date">
+        작성일
+        <span>: </span>
+        {state.time.substring(0, 4)}년<span> </span>
+        {state.time.substring(5, 7)}월<span> </span>
+        {state.time.substring(8, 10)}일<span> </span>
+        {state.time.substring(11, 13)}시<spna> </spna>
+        {state.time.substring(14, 16)}분
+      </p>
+      <div className="text">
+        {post.map((item) => {
+          if (item.substr(0, 7) === "(IMAGE)") {
+            return (
+              <Image
+                folder={"board"}
+                fileName={item.substr(7)}
+                style={{ width: "200px" }}
+              />
+            );
+          } else {
+            return <p className="comentText">{item}</p>;
+          }
+        })}
+      </div>
       <button
         className="updateButton"
         onClick={() => {
